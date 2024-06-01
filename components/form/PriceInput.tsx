2b7fd612d99106a1e7fee5d5a9f8a1b712b5cea0
Stephen Smith <stephensmith_aus@yahoo.com.au>
2024-06-02 08:13:27 +1000

    Properties - to Lesson 92

---
+++ components/form/PriceInput.tsx
@@ -0,0 +1,30 @@
+import { Prisma } from "@prisma/client";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+
+//const name = Prisma.PropertyScalarFieldEnum.price;
+
+type PriceInputProps = {
+  defaultValue?: number;
+};
+
+const PriceInput = ({ defaultValue }: PriceInputProps) => {
+  const name = 'price';
+
+  return (
+    <div className="mb-2">
+      <Label htmlFor="price" className="capitalize">
+        Price ($)
+      </Label>
+      <Input
+        id={name}
+        type="number"
+        name={name}
+        min={0}
+        defaultValue={defaultValue || 100}
+        required
+      />
+    </div>
+  );
+};
+export default PriceInput;
--- utils/actions.ts
+++ utils/actions.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
-import { imageSchema, profileSchema, validateWithZodSchema } from "./schemas";
+import { imageSchema, profileSchema, propertySchema, validateWithZodSchema } from "./schemas";
 import db from "./db";
 import { redirect } from "next/navigation";
 import { revalidatePath } from "next/cache";
@@ -138,4 +138,57 @@ export const updateProfileImageAction = async (
   } catch (error) {
     return renderError(error);
   }
+};
+
+export const createPropertyAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  const user = await getAuthUser();
+  try {
+    const rawData = Object.fromEntries(formData);
+    const file = formData.get('image') as File;
+    const validatedFields = validateWithZodSchema(propertySchema, rawData);
+    const validatedFile = validateWithZodSchema(imageSchema, {image: file});
+    const fullPath = await uploadImage(validatedFile.image);
+
+    await db.property.create({
+      data: {
+        ...validatedFields,
+        image: fullPath,
+        profileId: user.id,
+      },
+    });
+  } catch (error) {
+    return renderError(error);
+  }
+
+  redirect('/');
+};
+
+export const fetchProperties = async ({
+  search = '',
+  category,
+}: {
+  search?: string;
+  category?: string;
+}) => {
+  const properties = await db.property.findMany({
+    where: {
+      category,
+      OR: [
+        { name: { contains: search, mode: 'insensitive' } },
+        { tagline: { contains: search, mode: 'insensitive' } },
+      ],
+    },
+    select: {
+      id: true,
+      name: true,
+      tagline: true,
+      country: true,
+      image: true,
+      price: true,
+    },
+  });
+  return properties;
 };
